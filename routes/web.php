7ba6ba296eec013dc8cb282e17178994ccad85d2
unknown <dkdat12c1@gmail.com>
2020-07-14 00:08:44 +0700

    chuyen nhanh

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/index','HomeController@index' );
+Route::get('/','HomeController@index' );
 Route::get('user', function () {
     return view('user.user');
 });
