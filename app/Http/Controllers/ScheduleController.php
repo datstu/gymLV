8331ddc6b85621b0ba87261be95c3578121a8169
unknown <dkdat12c1@gmail.com>
2020-07-28 01:49:48 +0700

    dat lich pt

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -28,13 +28,7 @@ class ScheduleController extends Controller
         //lấy ra ds pt có lịch trống
        // $data['id_pt'] = $pt->id_pt;
         $data['id_gym'] = $gym->id_gym;
-        // echo "Thứ 2: " .$request->t2;
-        // echo "Thứ 3: ".$request->t3;
-        // echo "Thứ 4: ".$request->t4;
-        // echo "Thứ 5: ".$request->t5;
-        // echo "Thứ 6: ".$request->t6;
-        // echo "Thứ 7: ".$request->t7;
-        // echo "Thứ 8: ".$request->cn;
+   
         $data['thu2'] = $request->t2;
         $data['thu3'] = $request->t3;
         $data['thu4'] = $request->t4;
@@ -47,118 +41,140 @@ class ScheduleController extends Controller
         DB::table('tbl_schedule')->insert($data);
         $listpt = DB::table('tbl_personal_trainer')->get();
         $listSche = DB::table('tbl_schedule_pt')->get();
-        $listcus = DB::table('tbl_schedule')->where('id_users',$cus->id_user)->first();
-        echo "<pre>";
-        print_r($listcus);
-        echo "</pre>";
-      /*  foreach ($listpt as $key => $value) {
-        	if($value->status == 0){
-        		echo $value->name;
-        	}
-        }*/
-        // echo count($listSche);
-       /* $count = 1;
-			  		
-			  			while ($count <= 7) {
+        $listcus = DB::table('tbl_schedule')->where('id_users',$cus->id_user)
+        				->orderBy('id_schedule','desc')->first();
 
-			  				echo $count;
-			  				$count++;
-			  				
-			  			}*/
+        
+
        
-    		
+    	$arr = []; 
+        $temp = 0;
+        
+		foreach ($listcus as $key => $value) {
+        	if(is_string($value) )
+    		{
+	        		if($value != 'null'){
+	        			if($key == 'thu2'){
+	        				$temp = 2;
+	        			}else if($key == 'thu3'){
+	        				$temp = 3;
+	        			}else if($key == 'thu4'){
+	        				$temp = 4;
+	        			}else if($key == 'thu5'){
+	        				$temp = 5;
+	        			}else if($key == 'thu6'){
+	        				$temp = 6;
+	        			}else if($key == 'thu7'){
+	        				$temp = 7;
+	        			}else if($key == 'chunhat'){
+	        				$temp = 8;
+	        			}
+	        			
+	        			$arr[$temp] = $value;
+        		}
+        	}
 
-	  		if(count($listSche) > 0){
-	  			/*foreach ($listpt as $key => $value) {
-	        		if($value->status == 0){
-			  			
-		    		  		$listpt_thieu = DB::table('tbl_schedule_pt')
-		    		  		->where('id_pt',$value->id_pt)->get();
+        }//gom hết lịch tập của 1 khách hàng vào một mảng key là thứ, value là ca
+       
+       
+        //dd($arr);
+      
 
-		        		  	if($v->id_pt == $value->id_pt){
-		        		  		echo "string";
-		        		  	}
-		        		  	if(count($listpt_thieu) < 14){// số ca pt chưa đủ 14 ca 1 tuần thì thêm vào
-		        		  		foreach ($listpt_thieu as  $pt) {
-		        		  			if($pt->ca)
-		        		  		}
-		        		  		
-		        		  	}
+        	foreach ($listpt as $v) {
+        		if($v->status == 0){//kiểm tra xem đủ số ca quy đinh của pt chưa
+        			$data_pt['id_pt'] = $v->id_pt;
+        			$listptSche = DB::table('tbl_schedule_pt')->where('id_pt',$v->id_pt)->get();
+        			/*$s = count($listcus);
+        			echo $s;*/
+        			//dd($listptSche);
+			        $flag = false; 
+	        	foreach ($arr as $key => $value) {//duyệt lịch tập của khách hàng.
+	        		if(count($listptSche) == 0){//nếu pt chưa có lịch nào thì thêm vào lịch pt ngày mà khách đặt luôn
+
+	        			$data_pt['ca'] = $value;
+			  			$data_pt['thu'] = $key;
+			  			DB::table('tbl_schedule_pt')->insert($data_pt);
+
+	        			unset($arr[$key]);//đã thêm đc 1 ngày/lịch của pt tương ứng thì xóa mảng tạm đó
+	        							
 	        		
-			  		}
-		  		}
-		  		echo count($listpt_thieu);*/
-		  		echo "hihi";
-		  	// 	/*foreach ($listpt as $key => $value) {
-					// if($value->status == 0){
-			  // 			$data_pt['id_pt'] = $value->id_pt;
-			  			
-			  // 			$count = 1;
-			  // 		$listpt_thieu = DB::table('tbl_schedule_pt')
-		   //  		  		->where('id_pt',$value->id_pt)->get();
-		    		
-		   //  		/*echo "<pre>";
-     //    print_r($listpt_thieu);
-     //    echo "</pre>";*/
-     //    		while ( count($listpt_thieu) < 14) {// số ca pt chưa đủ 14 ca 1 tuần thì thêm vào
-		    		
-		   //  		foreach ($listpt_thieu as  $value_thieu) {
-		    			
-		   //  			if($value_thieu->thu == 2 && $value_thieu->ca != $request->t2 ){
-		   //  				$data_pt['ca'] = $request->t2;
-			  // 				$data_pt['thu'] = 2;
-			  // 				DB::table('tbl_schedule_pt')->insert($data_pt);
-		   //  			}
-		   //  			else if($value_thieu->thu == 3 && $value_thieu->ca != $request->t3 ){
-		   //  				$data_pt['ca'] = $request->t3;
-			  // 				$data_pt['thu'] = 3;
-			  // 				DB::table('tbl_schedule_pt')->insert($data_pt);
-		   //  			}
-		   //  			else if($value_thieu->thu == 4 && $value_thieu->ca != $request->t4 ){
-		   //  				$data_pt['ca'] = $request->t4;
-			  // 				$data_pt['thu'] = 4;
-			  // 				DB::table('tbl_schedule_pt')->insert($data_pt);
-		   //  			}
-		   //  			else if($value_thieu->thu == 5 && $value_thieu->ca != $request->t5 ){
-		   //  				$data_pt['ca'] = $request->t5;
-			  // 				$data_pt['thu'] = 5;
-			  // 				DB::table('tbl_schedule_pt')->insert($data_pt);
-		   //  			}
-		   //  			else if($value_thieu->thu == 6 && $value_thieu->ca != $request->t6 ){
-		   //  				$data_pt['ca'] = $request->t6;
-			  // 				$data_pt['thu'] = 6;
-			  // 				DB::table('tbl_schedule_pt')->insert($data_pt);
-		   //  			}
-		   //  			else if($value_thieu->thu == 7 && $value_thieu->ca != $request->t7 ){
-		   //  				$data_pt['ca'] = $request->t7;
-			  // 				$data_pt['thu'] = 7;
-			  // 				DB::table('tbl_schedule_pt')->insert($data_pt);
-		   //  			}
-		   //  			else if($value_thieu->thu == 8 && $value_thieu->ca != $request->cn ){
-		   //  				$data_pt['ca'] = $request->cn;
-			  // 				$data_pt['thu'] = 8;
-			  // 				DB::table('tbl_schedule_pt')->insert($data_pt);
-		   //  			}
-		   //  		}
 
-		   //  			$listpt_thieu = DB::table('tbl_schedule_pt')
-		   //  		  		->where('id_pt',$value->id_pt)->get();
-		   //  		}
-		   //  		//echo $listpt_thieu->ca;
-		        		  		
-			  // 			/*while ($count <= 7) {
+			        }else if(count($listptSche) <= 14){
+	
+			        	foreach ($listptSche as $ptSche) {
+			        		if($ptSche->thu > $key){
+			        			break;
+
+				        		}else if( $ptSche->thu == $key )
+				        		{
+					        		if( $ptSche->ca != $value)//cùng 1 thứ nhưng khác ca
+					        			{ 
+					        				$data_pt['ca'] = $value;
+							  				$data_pt['thu'] = $key;
+							  				DB::table('tbl_schedule_pt')->insert($data_pt);
+
+					        				unset($arr[$key]);
+					        			$listptSche = DB::table('tbl_schedule_pt')->where('id_pt',$v->id_pt)->get();	
+					        			if(count($listptSche) == 14){
+					        				//echo count($listptSche);
+			        						DB::table('tbl_personal_trainer')
+								            ->where('id_pt', $v->id_pt)
+								            ->update(['status' => 1]);
+								            		
+	        							}
+					        		$flag = false;
+					        		
+					        					break;
+					        				
+					        				
+					        		} else{
+					        			$flag = false;
+					        					break;
+					        		}
+				        		}else{
+				        			$flag = true;
+				        		}
+
+			        			
+
+			        	
+			        	}
+		       		
+			        	if($flag == true){
+			        		
+			        		$flag = false;
+
+			        		$data_pt['ca'] = $value;
+			  				$data_pt['thu'] = $key;
+			  				DB::table('tbl_schedule_pt')->insert($data_pt);
+			        		unset($arr[$key]);
+			        			$listptSche = DB::table('tbl_schedule_pt')->where('id_pt',$v->id_pt)->get();
+			        		if(count($listptSche) == 14){
+	        						DB::table('tbl_personal_trainer')
+						            ->where('id_pt', $v->id_pt)
+						            ->update(['status' => 1]);
+						            		
+	        					}
+	        							
+			        	}
+			        
+			       //đặt cờ: kiểm tra pt có ca nào chưa (lịch của pt có trước đó không trùng lịch khách hàng đặt hiện tại) thì flag = false cho đến khi trùng thứ thì kiểm tra có trùng ca ko. 
+					        		//+nếu trùng ca thì bỏ qua  flag = false và ko làm j
+					        		//+ko trùng ca thì thêm vào lịch, dừng vòng lặp và đặt lại flag = false
+					        		//trường hợp duyệt hết lịch của pt mà ko trùng lịch khách hàng đăng kí( 1ca / ngày) thì flag = true. Kiểm tra flag true thì thêm vào.	
+
+        		}
+        	}
+        		 
+
+				
+        	}
+        
+        }
 
 
 
-			  // 				echo $count;
-			  // 				$count++;
-			  				
-			  // 			}
-			  // 			*/
-	    //     			}*/
-        			//}	
-		  		
-	        }else{
+	        /*}else{
 	        	
 				foreach ($listpt as $key => $value) {
 					if($value->status == 0){
@@ -178,10 +194,11 @@ class ScheduleController extends Controller
 			  				echo $key;
 			  			}*/
 
-			  			if($listcus->thu2){
+			  		/*	if($listcus->thu2){
 			  				$data_pt['ca'] = $request->t2;
 			  				$data_pt['thu'] = 2;
 			  				DB::table('tbl_schedule_pt')->insert($data_pt);
+
 			  				echo "<pre>";
 				        print_r($data_pt);
 				        echo "</pre>";
@@ -246,7 +263,7 @@ class ScheduleController extends Controller
 			  			break;
 	        			}
         			}	
-        		}
+        		}*/
         		 // echo "<pre>";
 				       //  print_r($data_pt);
 				       //  echo "</pre>";
