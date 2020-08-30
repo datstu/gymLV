c0c46c06fc30c82c6f40afa1bebba4e8e4055369
unknown <dkdat12c1@gmail.com>
2020-08-30 16:56:25 +0700

    update thong ke

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -25,6 +25,62 @@ class AdminController extends Controller
         }
 
     }*/
+    public function chartOrder(){
+        //tổng các đơn hàng của trong 6 tháng từ tháng hiện tại trừ 1 đến tháng thứ 6
+        $date = date('Y-m-j');
+        $newdate = strtotime ( '-6 month' , strtotime ( $date ) ) ;
+        $newdate = date ( 'm' , $newdate );
+        //echo $newdate."<br>";
+        $listOrder = DB::table('tbl_order')->get();
+           $s = 0;
+        $result = array();
+        foreach ($listOrder as $key => $value) {
+            //if($value->date )
+            //echo date ( 'm' , $value->order_date );
+            //echo $value->order_date ."<br>";
+            $dateOder = strtotime (  $value->order_date ) ;
+        $dateOder = date ( 'm' , $dateOder );
+        //echo $dateOder."<br>";
+        $date = date('m');
+     
+            if(($dateOder+1) >= $newdate && $dateOder < $date){
+                //echo $value->order_date."<br>";
+                //$s += $value->totalPrice;
+                if(empty($result)){
+                    $result[$dateOder] = $value->totalPrice;
+                }else{
+                    if(isset($result[$dateOder])){
+                        $result[$dateOder] +=$value->totalPrice;
+                    }else{
+                        $result[$dateOder] = $value->totalPrice;
+                    }
+                }
+            }
+            /*nếu mảng rỗng khởi tạo giá trị đầu tiên cho mảng
+            nếu đã có mảng tiếp tục kiểm tra phần tử trong đó có ko 
+                nếu chưa có  thêm nó vào mảng
+                nếu đã có cộng thêm*/
+            
+        }
+        ksort($result);
+    
+        $arr_key_month = array_keys($result) ;
+        $arr_value_month = array_values($result) ;
+        $s= 0;
+        $arr_value_fm = array();
+        foreach ($arr_value_month as $key => $value) {
+            $s = ($value  * 100)/array_sum($arr_value_month);
+            //echo $key."<br>";
+            $arr_value_fm[$key] = number_format($s);
+            // echo $s."<br>";
+        }
+        //dd($arr_value_fm);
+        $keyJSON = json_encode($arr_key_month);
+         $valJSON = json_encode($arr_value_fm);
+         return [$keyJSON,$valJSON,$newdate-01,$date-01];
+         // tách ra hàm riêng
+         //gửi các giá trị là key, value, tháng hiện tại -1, và 6 tháng trước đó
+    }
     public function index(){
     	
     	$this->CheckLoginAdmin();
@@ -73,7 +129,19 @@ class AdminController extends Controller
         }
         $countVisitor = DB::table('tbl_visitor')->sum('viewNumber');
 
-    	return view('admin.dashboard')->with('countOrder',$countOrder)->with('countMember',$countMember)->with('totalPrice',$totalPrice)->with('countVisitor',$countVisitor);;
+
+        $arrCharOrder = $this->chartOrder();
+
+        //lấy các giá trị cần tìm bên hàm chart order
+        $valJSON = $arrCharOrder[1];//tổng số tiền trong 1 tháng
+        $keyJSON = $arrCharOrder[0];//tháng
+        $month_1 = $arrCharOrder[2];//tháng hiện tại - 6
+        $month_6 = $arrCharOrder[3];//tháng hiện tại - 1
+        
+        // echo $month_1;
+    	return view('admin.dashboard')->with('countOrder',$countOrder)->with('countMember',$countMember)->with('totalPrice',$totalPrice)->with('countVisitor',$countVisitor)
+        ->with('valJSON',$valJSON)->with('keyJSON',$keyJSON)->with('month_1',$month_1)
+        ->with('month_6',$month_6);
     	
     	
     		
